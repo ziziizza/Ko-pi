0c0d31ecef2b524a1660bb0ffcd36de0275368c9
ziziizza <ziaesma2011@gmail.com>
2025-02-25 07:25:59 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
